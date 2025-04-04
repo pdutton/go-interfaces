bd974e6c0796ab03a0fc99789480eb60a3c345ee
Peter Dutton <pdutton0@gmail.com>
2025-04-03 20:44:18 -0600

    expose functions

---
+++ net/functions.go
@@ -0,0 +1,13 @@
+package net
+
+import (
+    "net"
+)
+
+var (
+    JoinHostPort  = net.JoinHostPort
+    ParseCIDR     = net.ParseCIDR
+    Pipe          = net.Pipe
+    SplitHostPort = net.SplitHostPort
+)
+
