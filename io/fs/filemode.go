2751a6c427ed371c6df20661b8676140f12b60ac
Peter Dutton <pdutton0@gmail.com>
2025-06-13 13:03:17 -0600

    add Is* methods

--- io/fs/filemode.go
+++ io/fs/filemode.go
@@ -26,8 +26,21 @@ const (
 )
 
 type FileMode interface {
-    IsDir() bool
     IsRegular() bool
+    IsDir() bool
+    IsAppend() bool
+    IsExclusive() bool
+    IsTemporary() bool
+    IsSymlink() bool
+    IsDevice() bool
+    IsNamedPipe() bool
+    IsSocket() bool
+    IsSetuid() bool
+    IsSetgid() bool
+    IsCharDevice() bool
+    IsSticky() bool
+    IsIrregular() bool
+    
     Perm() FileMode
     String() string
     Type() FileMode
@@ -43,12 +56,60 @@ func newFileMode(fm fs.FileMode) fileModeFacade {
     }
 }
 
-func (fm fileModeFacade) IsDir() bool {
-    return fm.realFileMode.IsDir()
+func (fm fileModeFacade) IsRegular() bool {
+	return uint32(fm.realFileMode.Type()) & uint32(ModePerm) == 0
 }
 
-func (fm fileModeFacade) IsRegular() bool {
-    return fm.realFileMode.IsRegular()
+func (fm fileModeFacade) IsDir() bool {
+	return fm.realFileMode.IsDir()
+}
+
+func (fm fileModeFacade) IsAppend() bool {
+	return uint32(fm.realFileMode.Type()) & uint32(ModeAppend) != 0
+}
+
+func (fm fileModeFacade) IsExclusive() bool {
+	return uint32(fm.realFileMode.Type()) & uint32(ModeExclusive) != 0
+}
+
+func (fm fileModeFacade) IsTemporary() bool {
+	return uint32(fm.realFileMode.Type()) & uint32(ModeTemporary) != 0
+}
+
+func (fm fileModeFacade) IsSymlink() bool {
+	return uint32(fm.realFileMode.Type()) & uint32(ModeSymlink) != 0
+}
+
+func (fm fileModeFacade) IsDevice() bool {
+	return uint32(fm.realFileMode.Type()) & uint32(ModeDevice) != 0
+}
+
+func (fm fileModeFacade) IsNamedPipe() bool {
+	return uint32(fm.realFileMode.Type()) & uint32(ModeNamedPipe) != 0
+}
+
+func (fm fileModeFacade) IsSocket() bool {
+	return uint32(fm.realFileMode.Type()) & uint32(ModeSocket) != 0
+}
+
+func (fm fileModeFacade) IsSetuid() bool {
+	return uint32(fm.realFileMode.Type()) & uint32(ModeSetuid) != 0
+}
+
+func (fm fileModeFacade) IsSetgid() bool {
+	return uint32(fm.realFileMode.Type()) & uint32(ModeSetgid) != 0
+}
+
+func (fm fileModeFacade) IsCharDevice() bool {
+	return uint32(fm.realFileMode.Type()) & uint32(ModeCharDevice) != 0
+}
+
+func (fm fileModeFacade) IsSticky() bool {
+	return uint32(fm.realFileMode.Type()) & uint32(ModeSticky) != 0
+}
+
+func (fm fileModeFacade) IsIrregular() bool {
+	return uint32(fm.realFileMode.Type()) & uint32(ModeIrregular) != 0
 }
 
 func (fm fileModeFacade) Perm() FileMode {
