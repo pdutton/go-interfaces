7373af2456e8e77010f081c280840484a4fce84a
Peter Dutton <pdutton0@gmail.com>
2025-08-06 14:22:35 -0600

    add interface to os/signal (#16)

---
+++ os/signal/signal.go
@@ -0,0 +1,47 @@
+package signal
+
+import (
+	"context"
+	"os"
+	"os/signal"
+)
+
+type Signal interface {
+	Ignore(sig ...os.Signal)
+	Ignored(sig os.Signal) bool
+	Notify(c chan<- os.Signal, sig ...os.Signal)
+	NotifyContext(parent context.Context, signals ...os.Signal) (ctxt context.Context, stop context.CancelFunc)
+	Reset(sig ...os.Signal)
+	Stop(c chan<-os.Signal)
+}
+
+type signalFacade struct{}
+
+func NewSignal() Signal {
+	return signalFacade{}
+}
+
+func (_ signalFacade) Ignore(sig ...os.Signal) {
+	signal.Ignore(sig...)
+}
+
+func (_ signalFacade) Ignored(sig os.Signal) bool {
+	return signal.Ignored(sig)
+}
+
+func (_ signalFacade) Notify(c chan<- os.Signal, sig ...os.Signal) {
+	signal.Notify(c, sig...)
+}
+
+func (_ signalFacade) NotifyContext(parent context.Context, signals ...os.Signal) (ctxt context.Context, stop context.CancelFunc) {
+	return signal.NotifyContext(parent, signals...)
+}
+
+func (_ signalFacade) Reset(sig ...os.Signal) {
+	signal.Reset(sig...)
+}
+
+func (_ signalFacade) Stop(c chan<-os.Signal) {
+	signal.Stop(c)
+}
+
